43d96e680ab53017a6d9a2a7e27db961b7efed33
EmilioCrespoPeran <emiliocrespoperan@gmail.com>
2022-09-06 14:17:23 +0200

    Added XML validator

---
+++ src/main/java/helio/actions/validator/XmlValidatorAction.java
@@ -0,0 +1,45 @@
+package helio.actions.validator;
+
+import com.google.gson.JsonObject;
+
+import helio.blueprints.Action;
+import helio.blueprints.exceptions.ActionException;
+import org.xml.sax.InputSource;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.StringReader;
+
+/**
+ * Action class for Validator Module encharge of validating XML schemas.
+ * Return a json as String value with the following structure:
+ * - "status": "ok" if it is a valid json or "error" instead.
+ * - "message" (Optional). A status description used for "error" status.
+ *
+ * @author Emilio
+ *
+ */
+public class XmlValidatorAction implements Action {
+
+	@Override
+	public void configure(JsonObject configuration) {}
+
+	@Override
+	public String run(String values) throws ActionException {
+		JsonObject response = new JsonObject();
+		try {
+			DocumentBuilderFactory fctr = DocumentBuilderFactory.newInstance();
+			DocumentBuilder bldr = fctr.newDocumentBuilder();
+			InputSource insrc = new InputSource(new StringReader(values));
+			bldr.parse(insrc);
+			response.addProperty("status", "ok");
+		}
+		catch (Exception e) {
+			response.addProperty("status", "error");
+			response.addProperty("message", "Invalid XML format: " + e.getMessage());
+		}
+
+		return response.toString();
+	}
+
+}
